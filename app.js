a50ee30ad67a071fbbbbf1fe9e8967b3cbe65c2b
Tomohisa Uchida <uddystyle@gmail.com>
2018-07-02 12:02:27 +0900

    Implemted show page

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ var app = express()
 mongoose.connect('mongodb://localhost/restful_blog_app')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  created: {type: Date, default: Date.now}
+  created: {
+    type: Date,
+    default: Date.now
+  }
 })
 var Blog = mongoose.model('Blog', blogSchema)
 
@@ -34,7 +39,9 @@ app.get('/blogs', function (req, res) {
     if (err) {
       console.log("ERROR!")
     } else {
-      res.render('index', {blogs: blogs})
+      res.render('index', {
+        blogs: blogs
+      })
     }
   })
 })
@@ -45,6 +52,30 @@ app.get('/blogs/new', function (req, res) {
 })
 
 // CREATE ROUTE
+app.post('/blogs', function (req, res) {
+  // create blog
+  Blog.create(req.body.blog, function (err, newBlog) {
+    if (err) {
+      res.render('new')
+    } else {
+      // then, redirect to the index
+      res.redirect('/blogs')
+    }
+  })
+})
+
+// SHOW ROUTE
+app.get('/blogs/:id', function (req, res) {
+  Blog.findById(req.params.id, function (err, foundBlog) {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.render('show', {
+        blog: foundBlog
+      })
+    }
+  })
+})
 
 app.listen(3000, function () {
   console.log('SERVER IS RUNNING!!')
